39fb5676a2af3c1404fc79c4849d79b90fe03091
Devpareek004 <165404806+Devpareek004@users.noreply.github.com>
2024-11-19 08:17:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'samra')
 API_ID = int(environ.get('API_ID', '24942826'))
 API_HASH = environ.get('API_HASH', 'e3e2f3b65ef58634139ccd27d6b7d8cb')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7453674332:AAEVJU6UJd7XL_JaUEboQMJgoRpqv83kMNk")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7453674332:AAGwl2taVw81Osw09ZDQC-OY6CAsQk99OAU")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
